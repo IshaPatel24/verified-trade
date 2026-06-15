7d45aa2d1ca353cb3651261902f4a82e4619dd94
IshaPatel24 <ip150772@gmail.com>
2026-06-15 13:02:56 +0530

    Revert outputFileTracing hack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracing: false,
   env: {
     NEXT_PUBLIC_CLEANVERSE_APP_ID: process.env.NEXT_PUBLIC_CLEANVERSE_APP_ID,
     NEXT_PUBLIC_CLEANVERSE_DOCS_CODE: process.env.NEXT_PUBLIC_CLEANVERSE_DOCS_CODE,
